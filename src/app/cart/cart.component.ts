d0cb002744a843535c743dda08f172736ff0b9ac
sntgzrr <santilozanoc077@gmail.com>
2024-01-21 13:37:56 -0500

    Eliminar item carrito

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,21 +7,38 @@ import { CartService, Cart } from 'src/services/cart.service';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit{
+export class CartComponent implements OnInit {
   shoe: Shoe = new Shoe;
   carts: Cart[] = [];
   sumPrice: number = 0;
-  
-  constructor(private cartService: CartService) {}
 
-    ngOnInit(): void {
-      this.carts = this.cartService.obtenerCarritos();
-      console.log(this.carts);
-      for (let i = 0; i < this.carts.length; i++) {
-        const priceAsNumber = Number(this.carts[i].finalprice);
-        if (!isNaN(priceAsNumber)) {
-          this.sumPrice += priceAsNumber;
-        }
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.carts = this.cartService.obtenerCarritos();
+    console.log(this.carts);
+    for (let i = 0; i < this.carts.length; i++) {
+      const priceAsNumber = Number(this.carts[i].finalprice);
+      if (!isNaN(priceAsNumber)) {
+        this.sumPrice += priceAsNumber;
+      }
+    }
+  }
+
+  eliminarItem(index: number): void {
+    if (index >= 0 && index < this.carts.length) {
+      this.carts.splice(index, 1);
+      this.actualizarSumaPrecios();
+    }
+  }
+
+  private actualizarSumaPrecios(): void {
+    this.sumPrice = 0;
+    for (let i = 0; i < this.carts.length; i++) {
+      const priceAsNumber = Number(this.carts[i].finalprice);
+      if (!isNaN(priceAsNumber)) {
+        this.sumPrice += priceAsNumber;
+      }
     }
   }
 
